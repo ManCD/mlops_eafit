ed4b57148f0cbaf0d9111e85493946af17c7c92c
Manuel <ingmanuelandradef@gmail.com>
2024-02-27 11:44:17 -0500

    My Workflow MLOPS-Regresion-git

--- model2/simple_regresion.py
+++ model2/simple_regresion.py
@@ -20,7 +20,6 @@ warnings.filterwarnings("ignore", category=ConvergenceWarning)
 """## Step 0: Install W&B"""
 #!pip install wandb -qU
 """## Step 1: Import W&B and Login"""
-
 import wandb
 
 wandb.login()
