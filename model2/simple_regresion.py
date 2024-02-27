fb166968ef0b7f4eb03f9bb14e937ba031905578
Manuel <ingmanuelandradef@gmail.com>
2024-02-27 11:17:00 -0500

    My Workflow MLOPS-Regresion-git

--- model2/simple_regresion.py
+++ model2/simple_regresion.py
@@ -50,7 +50,6 @@ run = wandb.init(project='my-scikit-integration', name="regression_git")
 """## Step 3: Visualize model performance
 """
 wandb.sklearn.plot_residuals(reg, X_train, y_train)
-
 wandb.sklearn.plot_outlier_candidates(reg, X_train, y_train)
 wandb.sklearn.plot_regressor(reg, X_train, X_test, y_train, y_test, model_name='Ridge')
 wandb.finish()
